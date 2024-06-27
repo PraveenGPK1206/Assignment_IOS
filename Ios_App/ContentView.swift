5930c31d0c54e5a59cbab6cd82b42be263c13373
PraveenGPK1206 <kumargundrala.com>
2024-06-27 14:23:09 +0530

    Initial Commit

---
+++ Ios_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ios_App
+//
+//  Created by Praveen on 27/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ios_App/Ios_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_AppApp.swift
+//  Ios_App
+//
+//  Created by Praveen on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Ios_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
